ba8d2c453b3496447db98d0917111aed4dae1736
Jeff Hodges <jeff@somethingsimilar.com>
2019-10-17 02:09:10 -0700

    try old one

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@
 set -euo pipefail
 
 GO_VERSION=$(latest_go_ensurer $@)
-echo "GO_VERSION though? ${GO_VERSION}"
-echo ::set-output name=go_version::$GO_VERSION
+echo ##[set-output name=go_version]$GO_VERSION
